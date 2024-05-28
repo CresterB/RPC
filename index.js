9df72058f12a465b0e026c50f50956777e1f7c78
CresterB <goel20bhavya@gamil.com>
2024-05-28 05:45:57 +0530

    removed unused imports

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const client = new Client({
 const env = require("dotenv");
 env.config();
 const keep_alive = require("./keep_alive.js");
-const tokenSchema = require("./models/tokenschema.js");
 
 client.on("ready", async () => {
   console.log(`${client.user.username} is ready!`);
@@ -26,18 +25,6 @@ client.on("ready", async () => {
       "https://cdn.discordapp.com/attachments/1188924983666282517/1244216851999883264/playstation-icon-2048x1665-icu9gzjj.png?ex=665648fd&is=6654f77d&hm=5f500feafaf8c99516c8e79238b6157ce3d730cb4384e9d65be2f87dd2d10cf2&"
     );
   client.user.setActivity(r);
-
-  (async () => {
-    const newToken = new tokenSchema({
-      data: {
-        CLIENT_ID: process.env.CLIENT_ID,
-        token: process.env.TOKEN,
-      },
-    });
-    await newToken.save().catch((e) => {
-      console.log(e);
-    });
-  })();
 
   setInterval(() => {
     console.log("In interval");
