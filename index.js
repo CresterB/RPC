56cfee1ccf8af5b4362d92892e877abab3247cf3
CresterB <goel20bhavya@gamil.com>
2024-05-30 09:18:18 +0530

    changed images url

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ client.on("ready", async () => {
     .setName("Grand Theft Auto VI")
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage(
-      "https://cdn.discordapp.com/attachments/1188924983666282517/1244216128046239796/dgj5wme-327561a1-f8f9-41bd-b591-57ba244348c2.png?ex=66564851&is=6654f6d1&hm=6a51f97b6bcedd87dcc94fd0c382e234ad744a4a98adc3d7e73fb2306de5bcfa&"
+      "https://cdn.discordapp.com/attachments/1188924983666282517/1244216128046239796/dgj5wme-327561a1-f8f9-41bd-b591-57ba244348c2.png?ex=6658eb51&is=665799d1&hm=a32efbd8ca91be9cbe252651af54d243740b334a6bfbdf4287235d792d811dbb&"
     )
     .setAssetsSmallImage(
-      "https://cdn.discordapp.com/attachments/1188924983666282517/1244216851999883264/playstation-icon-2048x1665-icu9gzjj.png?ex=665648fd&is=6654f77d&hm=5f500feafaf8c99516c8e79238b6157ce3d730cb4384e9d65be2f87dd2d10cf2&"
+      "https://cdn.discordapp.com/attachments/1188924983666282517/1244216851999883264/playstation-icon-2048x1665-icu9gzjj.png?ex=6658ebfd&is=66579a7d&hm=366f9274820fd7958eb08edb41d60c4dac0561654ca45b57c5f097e17bc88185&"
     );
   client.user.setActivity(r);
 
