643a3523ed1bc5381f9983c481b101441b348a54
CresterB <goel20bhavya@gamil.com>
2024-07-02 04:54:23 +0530

    initial commit

---
+++ events/messageCreate/checkmessage.js
@@ -0,0 +1,29 @@
+const ids = new Set();
+let afk = true;
+
+module.exports = (message, client) => {
+  if (message.author.id == "211032990195777537" && message.content == "?afk") {
+    afk = !afk;
+    message.channel.sent(
+      afk ? "You have been set to afk" : "You are not afk anymore"
+    );
+  }
+  if (
+    message.channel.type !== "DM" ||
+    client.user.id === message.author.id ||
+    ids.has(`${message.author.id}`)
+  )
+    return;
+
+  if (afk) {
+    const msg =
+      message.author.id == "937841459451793509"
+        ? `Hi bb this is an automate message, I am afk right now could be sleeping. I will respond back to you asap or you can call me`
+        : "This is an automate message, I am afk right now and if you have any urgent work DO IT YOURSLEF 🙂";
+    message.channel.send(msg);
+    ids.add(`${message.author.id}`);
+    setTimeout(() => {
+      ids.delete(`${message.author.id}`);
+    }, 60 * 1000);
+  }
+};
--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const { Client, RichPresence } = require("discord.js-selfbot-v13");
+const { Client } = require("discord.js-selfbot-v13");
 const client = new Client({
   readyStatus: false,
   checkUpdate: false,
 });
 const env = require("dotenv");
 env.config();
+const { CommandKit } = require("commandkit");
+const path = require("path");
 const keep_alive = require("./keep_alive.js");
 
-client.on("ready", async () => {
-  console.log(`${client.user.username} is ready!`);
-  client.user.bio = "bio from bot";
+// client.on("ready", async () => {
+//   console.log(`${client.user.username} is ready!`);
+//   client.user.bio = "bio from bot";
 
-  const r = new RichPresence()
-    .setApplicationId(process.env.CLIENT_ID)
-    .setType("PLAYING")
-    .setState("Playing on Playstation")
-    .setDetails("Beta Tester Edition")
-    .setName("Grand Theft Auto VI")
-    .setStartTimestamp(Date.now())
-    .setAssetsLargeImage(
-      "https://media.discordapp.net/attachments/1188924983666282517/1244216128046239796/dgj5wme-327561a1-f8f9-41bd-b591-57ba244348c2.png?ex=665eda11&is=665d8891&hm=aed49e14a7b617415bc06771b5b00501156d2961f25725478f04e5b510d74f89&=&format=webp&quality=lossless"
-    )
-    .setAssetsSmallImage(
-      "https://cdn.discordapp.com/attachments/1188924983666282517/1244216851999883264/playstation-icon-2048x1665-icu9gzjj.png?ex=665edabd&is=665d893d&hm=21e3a40b1fa679f50cf49ee10200067463dcc25c9ac3c7ecbe195f4cec914c1b&"
-    );
-  client.user.setActivity(r);
+//   const r = new RichPresence()
+//     .setApplicationId(process.env.CLIENT_ID)
+//     .setType("PLAYING")
+//     .setState("Playing on Playstation")
+//     .setDetails("Beta Tester Edition")
+//     .setName("Grand Theft Auto VI")
+//     .setStartTimestamp(Date.now())
+//     .setAssetsLargeImage(
+//       "https://media.discordapp.net/attachments/1188924983666282517/1244216128046239796/dgj5wme-327561a1-f8f9-41bd-b591-57ba244348c2.png?ex=665eda11&is=665d8891&hm=aed49e14a7b617415bc06771b5b00501156d2961f25725478f04e5b510d74f89&=&format=webp&quality=lossless"
+//     )
+//     .setAssetsSmallImage(
+//       "https://cdn.discordapp.com/attachments/1188924983666282517/1244216851999883264/playstation-icon-2048x1665-icu9gzjj.png?ex=665edabd&is=665d893d&hm=21e3a40b1fa679f50cf49ee10200067463dcc25c9ac3c7ecbe195f4cec914c1b&"
+//     );
+//   client.user.setActivity(r);
 
-  setInterval(() => {
-    console.log("In interval");
-    client.user.setActivity(r);
-  }, 15 * 60 * 1000);
-  client.user.setPresence({ status: "dnd" });
+//   setInterval(() => {
+//     console.log("In interval");
+//     client.user.setActivity(r);
+//   }, 15 * 60 * 1000);
+//   client.user.setPresence({ status: "dnd" });
+// });
+
+new CommandKit({
+  client,
+  eventsPath: path.join(__dirname, "events"),
+  bulkRegister: true,
 });
 
 client.login(process.env.TOKEN);
