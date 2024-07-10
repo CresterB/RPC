7c5a5d15fedf92dc60c1ff5b63166c1aeaa4c271
CresterB <goel20bhavya@gamil.com>
2024-07-10 05:55:56 +0530

    updated images

--- events/ready/rpc.js
+++ events/ready/rpc.js
@@ -10,10 +10,10 @@ module.exports = (c, client, handler) => {
     .setName("Grand Theft Auto VI")
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage(
-      "https://media.discordapp.net/attachments/1188924983666282517/1244216128046239796/dgj5wme-327561a1-f8f9-41bd-b591-57ba244348c2.png?ex=66846cd1&is=66831b51&hm=9473d33839d746a5e75272fd33013364075bfa4a01eb3f64806d7e71e4a5e27f&=&format=webp&quality=lossless"
+      "https://media.discordapp.net/attachments/1188924983666282517/1244216128046239796/dgj5wme-327561a1-f8f9-41bd-b591-57ba244348c2.png?ex=668ef8d1&is=668da751&hm=6a48d2713e0b22811ea84d622795cb3332617367405681660426f997c2bf0011&=&format=webp&quality=lossless"
     )
     .setAssetsSmallImage(
-      "https://media.discordapp.net/attachments/1188924983666282517/1244216851999883264/playstation-icon-2048x1665-icu9gzjj.png?ex=66846d7d&is=66831bfd&hm=4bda95f8fd2988af05c3449f2ae95c96671e4915e8c03d14748a5d982a0165de&=&format=webp&quality=lossless&width=825&height=671"
+      "https://media.discordapp.net/attachments/1188924983666282517/1244216851999883264/playstation-icon-2048x1665-icu9gzjj.png?ex=668ef97d&is=668da7fd&hm=e3bee5bd8f70bf5a9bdb59ceab94ee67aefe3ae1e3be4952ba41260b0e01538e&=&format=webp&quality=lossless&width=431&height=350"
     );
   client.user.setActivity(r);
 
