c0947d874334b680c743d08b73da2b180469630b
CresterB <38030961+CresterB@users.noreply.github.com>
2025-01-09 01:57:15 +0530

    Update rpc.js

--- events/ready/rpc.js
+++ events/ready/rpc.js
@@ -1,4 +1,4 @@
 module.exports = (c, client, handler) => {
   console.log(`${c.user.username} is ready!`);
-  client.user.setPresence({ status: 'idle' });
+  client.user.setPresence({ status: 'dnd' });
 };
