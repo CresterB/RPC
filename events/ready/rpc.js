c1424d22a9d9a8e8e2bcf4f92d5442191fb54a5e
CresterB <38030961+CresterB@users.noreply.github.com>
2025-01-09 01:52:14 +0530

    Update rpc.js

--- events/ready/rpc.js
+++ events/ready/rpc.js
@@ -1,19 +1,4 @@
-const { Client } = require('discord.js-selfbot-v13');
-const client = new Client({
-  readyStatus: false,
-  checkUpdate: false,
-});
-
-const env = require('dotenv');
-env.config();
-const { CommandKit } = require('commandkit');
-const path = require('path');
-const keep_alive = require('./keep_alive.js');
-
-new CommandKit({
-  client,
-  eventsPath: path.join(__dirname, 'events'),
-  bulkRegister: true,
-});
-
-client.login(process.env.TOKEN);
+module.exports = (c, client, handler) => {
+  console.log(`${c.user.username} is ready!`);
+  client.user.setPresence({ status: 'idle' });
+};
